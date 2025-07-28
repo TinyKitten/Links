f5df43ae706410b6626cd86c65785b650b5a871e
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-07-29 08:07:38 +0900

    v0.1くらい

---
+++ src/app/components/LinkButton.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  color: string;
+  label: string;
+  href: string;
+};
+
+export const LinkButton = ({
+  className,
+  color,
+  label,
+  href,
+  ...props
+}: React.ComponentProps<typeof Link> & Props) => {
+  return (
+    <Link
+      href={href}
+      type="button"
+      target="_blank"
+      {...props}
+      className={twMerge(
+        `block bg-white w-full p-0 text-left border-l-12 rounded-sm px-4 py-3 shadow cursor-pointer hover:shadow-lg transition-shadow border-l-[${color}]`,
+        className
+      )}
+    >
+      <p className="font-bold max-sm:text-sm/4">{label}</p>
+      <p className="text-xs/4 font-semibold text-gray-500">{href}</p>
+    </Link>
+  );
+};
