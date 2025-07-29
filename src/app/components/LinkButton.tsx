79935c6fa594acbd28d196a395cdf3506e391dde
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-07-29 22:45:18 +0900

    hover挙動追加

--- src/app/components/LinkButton.tsx
+++ src/app/components/LinkButton.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
@@ -14,6 +17,8 @@ export const LinkButton = ({
   href,
   ...props
 }: React.ComponentProps<typeof Link> & Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <Link
       href={href}
@@ -21,13 +26,24 @@ export const LinkButton = ({
       target="_blank"
       {...props}
       className={twMerge(
-        `block bg-white w-full p-0 text-left border-l-16 rounded-sm px-4 py-3 shadow cursor-pointer hover:shadow-lg transition-shadow`,
+        `block relative flex items-center h-16 bg-white w-full p-0 text-left border-l-16 rounded-sm shadow cursor-pointer hover:shadow-lg transition-shadow`,
         className
       )}
       style={{ borderLeftColor: color }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <p className="font-bold max-sm:text-sm/4">{label}</p>
-      <p className="text-xs/4 font-semibold text-gray-500">{href}</p>
+      <div
+        className="absolute w-[calc(100%-2px)] h-full rounded-r-sm transition-opacity duration-300"
+        style={{
+          backgroundColor: color,
+          opacity: isHovered ? 1 : 0,
+        }}
+      />
+      <div className="relative flex flex-col justify-center rounded-r-[2px] pl-4 z-10 bg-white w-full mr-1 h-[calc(100%-4px)]">
+        <p className="font-bold max-sm:text-sm/4">{label}</p>
+        <p className="text-xs/4 font-semibold text-gray-500">{href}</p>
+      </div>
     </Link>
   );
 };
